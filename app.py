1e4edb034ff78a6860b82ec23e7006fc14943466
jeonginmo18 <cat10830@naver.com>
2022-04-15 15:58:12 +0900

    업로드 확인

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ def mnist():
         f = request.files['mnistfile']
         path = os.path.dirname(__file__) + '/upload/' + f.filename
         f.save(path)
-        img = Image.open(path).convert('L')
-        img = np.resize(img,(1,784))
+        # img = Image.open(path).convert('L')
+        # img = np.resize(img,(1,784))
         # img = 255 - (img)
         # mpath = os.path.dirname(__file__) + '/model1.pickle'
         # with open(mpath, 'rb') as f:
         #     model = pickle.load(f)
         # pred = model.predict(img)
-        return render_template('mnistresult.html', data=pred)
+        # return render_template('mnistresult.html', data=pred)
 
 if __name__ ==  '__main__':
     app.run(debug=True)
